460eb7e10c7acfa1b54efb1844ce2b5df122fe8c
yujiah-github <cil05265@naver.com>
2022-07-09 19:32:48 +0900

    Feat: View Development of App Component and Apply to Moive Open API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+        `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=18442cacd8ac1c4bb4a671248df4ff43&targetDt=20220706&itemPerPage=10`
+      )
+    const json = await response.json();
+    setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+      <h1>Loading...</h1> 
+      ) : (
+      <div>
+        {movies.map(movie => (
+          <div key={movie.rnum}>
+            <h2>{movie.movieNm}</h2>
+            <p>{movie.openDt}</p>
+            <p>{movie.rankInten}</p>
+          </div>
+        ))}
+      </div>
+      )}
     </div>
-  );
+)
 }
 
 export default App;
