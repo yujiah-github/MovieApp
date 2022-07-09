c410f0ecbdb34d6d86fe64c776b9723e96f94005
yujiah-github <cil05265@naver.com>
2022-07-10 02:17:27 +0900

    Feat: Import React Router and Apply React Router to the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
+import Home from '../src/routes/Home';
+import Detail from '../src/routes/Detail';
+
 function App() {
   return(
-    null
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 function Movie({rnum, movieNm, openDt, rank, rankInten}){
     return(
         <div key={rnum}>
-            <h2>{movieNm}</h2>
+            <Link to='/movie'>{movieNm}</Link>
             <p>{openDt}</p>
             <p>{rank}</p>
             <p>{rankInten}</p>
