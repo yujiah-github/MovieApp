258029324cc5ee5f552050ab556c5030f1783d31
yujiah-github <cil05265@naver.com>
2022-07-18 11:03:17 +0900

    Feat: Add NavBar In Home Component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from "react";
-import Movie from "../components/Movie";
+import Movie from "../components/DailyBoxOffice";
 
 function Home() {
+    /*
+    <div>
+            {movies.map((movie) => (
+                <Movie
+                key = {movie.rnum}
+                movieId={movie.movieCd}
+                movieNm={movie.movieNm}
+                openDt={movie.openDt}
+                rank = {movie.rank}
+                rankInten={movie.rankInten}
+                />
+            ))} 
+        </div>
+    */
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
     const getMovies = async () => {
@@ -22,18 +36,14 @@ return (
         {loading ? (
         <h1>Loading...</h1> 
         ) : (
-        <div>
-            {movies.map((movie) => (
-                <Movie
-                key = {movie.rnum}
-                movieId={movie.movieCd}
-                movieNm={movie.movieNm}
-                openDt={movie.openDt}
-                rank = {movie.rank}
-                rankInten={movie.rankInten}
-                />
-            ))} 
-        </div>
+        <nav>
+            <ul>
+                <li>일별 박스오피스</li>
+                <li>주간/주말 박스오피스</li>
+                <li>영화목록</li>
+                <li>영화사목록</li>
+            </ul>
+        </nav>
         )}
     </div>
 )
