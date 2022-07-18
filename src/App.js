6dbe66c90927b7dd4c3de2e5c60e22f17d007a27
yujiah-github <cil05265@naver.com>
2022-07-18 20:19:58 +0900

    Feat: Apply React Router in App Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import Home from '../src/routes/Home';
 import Detail from '../src/routes/Detail';
+import DailyBoxOffice from './pages/dailyBoxOfficePage/DailyBoxOffice';
+import WeeklyBoxOffice from './pages/weeklyBoxOfficePage/WeeklyBoxOffice';
+import Movie from './pages/moviePage/Movie';
+import MovieCompany from './pages/movieCompanyPage/MovieCompany';
 
 function App() {
   return(
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/movie/:movieId" element={<Detail />} />
         <Route path="/" element={<Home />} />
+        <Route path="/dailyboxoffice" element={<DailyBoxOffice />} />
+        <Route path="/weeklyboxoffice" element={<WeeklyBoxOffice />} />
+        <Route path="/movie" element={<Movie/>} />
+        <Route path="/movie/:movieId" element={<Detail />} />
+        <Route path="/moviecompany" element={<MovieCompany />} />
       </Routes>
     </Router>
   );
