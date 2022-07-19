ade5605e3a0831a3f685105873a9beb50f6ce15b
yujiah-github <cil05265@naver.com>
2022-07-19 10:48:52 +0900

    Feat: Add DateSelect Component, Add getNewDailyBoxOffice Function in WeeklyBoxOffice Page

--- src/pages/weeklyBoxOfficePage/WeeklyBoxOffice.js
+++ src/pages/weeklyBoxOfficePage/WeeklyBoxOffice.js
@@ -1,11 +1,17 @@
 import React from "react";
 import {useEffect, useState} from 'react';
 import WeeklyBoxOfficeList from "./WeeklyBoxOfficeList";
+import styled from 'styled-components';
+
+
+const DateSelect = styled.input`
+
+`;
 
 function WeeklyBoxOffice(){
     const [loading, setLoading] = useState(true);
     const [WeeklyBoxOffice, setWeeklyBoxOffice] = useState([]);
-    const getMovies = async() => {
+    const getWeeklyMovies = async() => {
         const response = await fetch(
             `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=18442cacd8ac1c4bb4a671248df4ff43&targetDt=20120101`
         )
@@ -15,8 +21,34 @@ function WeeklyBoxOffice(){
     };
     
     useEffect(() => {
-    getMovies();
+    getWeeklyMovies();
     }, []);
+
+    //검색 날짜 선택
+    const [dateValue, setDateValue] = useState("");
+
+    //검색 날짜 변경
+    const onChangeDateValue = (event) => {
+        setDateValue(event.target.value);
+    }
+
+    //선택한 날짜에 따라 api를 다시 불러오기
+    const getNewWeeklyMovies = async() => {
+        const response = await fetch(
+            `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=18442cacd8ac1c4bb4a671248df4ff43&targetDt=${dateValue}`
+        )
+        const json = await response.json();
+        setWeeklyBoxOffice(json.boxOfficeResult.weeklyBoxOfficeList);
+        setLoading(false);
+    };
+
+    const onDateSubmit = (event) => {
+        event.preventDefault();
+        /*useEffect(() => {
+            getNewWeeklyMovies();
+        }, []);
+        */
+    }
 
     return(
         <div>
@@ -25,6 +57,18 @@ function WeeklyBoxOffice(){
             )
                 : (
                     <div>
+                        <h1>주간 박스오피스 순위</h1>
+                        <h2>날짜 선택</h2>
+                        <DateSelect
+                            type="date"
+                            id="start"
+                            name="trip-start"
+                            value={dateValue}
+                            onChange={onChangeDateValue}
+                            required
+                        />
+                        <button onClick={onDateSubmit}>날짜 검색하기</button>
+
                     {WeeklyBoxOffice.map((moive) =>
                     <WeeklyBoxOfficeList 
                     key={moive.rnum}
