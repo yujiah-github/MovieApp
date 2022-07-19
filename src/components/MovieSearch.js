33476233ef54dc5b663112a0be59938e1e531d18
yujiah-github <cil05265@naver.com>
2022-07-19 15:28:19 +0900

    Design: Styling MovieSearch Component

---
+++ src/components/MovieSearch.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const MovieSearch = styled.form`
+    text-align: center;
+    font-size: 23px;
+    justify-items: center;
+    
+    select {
+        font-size: large;
+    }
+
+    option {
+        font-size: large;
+    }
+
+    input {
+        font-size: large;
+    }
+
+    button {
+        font-size: large;
+    }
+`;
